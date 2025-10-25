58bde62ddc2bdef9a283eacd9d951d9a9bbabd34
Luan Oliveira <olivrluan@gmail.com>
2025-10-25 01:47:55 -0300

    test(fixtures): adicionar fixture valid_poll e registrar listener before_commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
-from src.models import Base
+import pytest
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import sessionmaker, Session as SessionType
+
+from src.models import Base, Polls, PollsStatus, Options
+from src.db.events import before_commit_listener
 
 @pytest.fixture(scope="function")
 def session():
@@ -10,11 +13,30 @@ def session():
     engine = create_engine("sqlite:///:memory:", echo=False)
     Base.metadata.create_all(engine)
     
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
+    SessionLocal = sessionmaker(bind=engine)
+    session = SessionLocal()
+
+    event.listen(SessionLocal, 'before_commit', before_commit_listener)
+    # event.listen(Polls)
+
     yield session  
     
     session.rollback()
     session.close()
 
+
+@pytest.fixture(scope='function')
+def valid_poll():
+    valid_poll = Polls(
+            question="Qual a sua linguagem favorita?",
+            status=PollsStatus.NAO_INICIADO,
+            start_date=datetime(2025, 11, 24),
+            end_date=datetime(2025, 11, 25)
+        )
+    valid_poll.options = [
+        Options(text='Python'),
+        Options(text='Javascript'),
+        Options(text='Java'),
+    ]
+
+    return valid_poll
